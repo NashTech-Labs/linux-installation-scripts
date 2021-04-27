eb55000d54046bba685920167c1dfa72d012ef8c
Manas Kashyap <manas.kashyap@knoldus.com>
2021-04-27 16:33:57 +0530

    jenkins install script added

---
+++ jenkins_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
+    /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get -y install jenkins
