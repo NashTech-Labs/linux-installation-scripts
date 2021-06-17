1fd659d5fc038916a022cd0ca7fac9a897f8faf3
Manas Kashyap <Manas_Kashyap@ultimatesoftware.com>
2021-06-17 12:08:10 +0530

    trusting the source and overwriding gpg check

--- kafkacat_install.sh
+++ kafkacat_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "deb [signed-by=/usr/share/keyrings/unstable.gpg] http://deb.debian.org/debian unstable main" | sudo tee -a /etc/apt/sources.list.d/kafkacat.list
+echo "deb [trusted=yes] http://deb.debian.org/debian unstable main" | sudo tee -a /etc/apt/sources.list.d/kafkacat.list
 sudo apt-get install -y apt-transport-https ca-certificates gnupg
-curl http://ftp.debian.org/debian/dists/unstable/Release.gpg | sudo apt-key --keyring /usr/share/keyrings/unstable.gpg add -
+# curl http://ftp.debian.org/debian/dists/unstable/Release.gpg | sudo apt-key --keyring /usr/share/keyrings/unstable.gpg add -
 sudo apt-get -y update && sudo apt-get install -y kafkacat
 echo "=======INSTALLED KAFKACAT============"
 sudo rm /etc/apt/sources.list.d/kafkacat.list
