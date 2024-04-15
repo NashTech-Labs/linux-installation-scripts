ca46735bfceb3ccc0a16f135106dec2edf2b267a
aayushknoldus03 <aayush.bisht@nashtechglobal.com>
2024-04-15 14:59:28 +0530

    add superuser

--- logstash.sh
+++ logstash.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
+# Switch to superuser
+sudo su
+
 # Update package index
-sudo apt update
+apt update
 
 # Install Java Development Kit (JDK)
-sudo apt install -y default-jdk
+apt install -y default-jdk
 
 # Download and install Logstash
-wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-sudo sh -c 'echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" > /etc/apt/sources.list.d/elastic-7.x.list'
-sudo apt update
-sudo apt install -y logstash
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
+sh -c 'echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" > /etc/apt/sources.list.d/elastic-7.x.list'
+apt update
+apt install -y logstash
 
 # Start Logstash service
-sudo systemctl start logstash
+systemctl start logstash
 
 # Enable Logstash service to start on system boot
-sudo systemctl enable logstash
+systemctl enable logstash
 
 # Verify Logstash installation
-sudo systemctl status logstash
+systemctl status logstash
